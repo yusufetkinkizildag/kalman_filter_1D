162dc3689a92a0049a7032b0279b7345d7be6fbb
Yusuf Etkin KIZILDAĞ <49615906+yusufetkinkizildag@users.noreply.github.com>
2022-11-04 10:51:48 +0300

    Update kalman_filter_1D.cpp

--- src/cpp/src/kalman_filter_1D.cpp
+++ src/cpp/src/kalman_filter_1D.cpp
@@ -3,45 +3,33 @@
 namespace etkin
 {
 
-    KalmanFilter::KalmanFilter(KalmanFilterState const &state) noexcept
-        : state{state}
-        {
-
-        }
+    KalmanFilter::KalmanFilter(KalmanFilterState const &state) noexcept : state{state} {}
     
-    KalmanFilter::KalmanFilter(KalmanFilterState &&state) noexcept
-        : state{std::move(state)}
-        {
-
-        }
+    KalmanFilter::KalmanFilter(KalmanFilterState &&state) noexcept : state{std::move(state)} {}
 
     double KalmanFilter::update(double const current_measurement) noexcept
     {
-        this->state.current_measurement = current_measurement;
-        calculate_kalman_gain();
-        calculate_current_estimate();
-        calculate_error_in_estimate();
-        return this->state.current_estimate;
+        auto &s{this->state};
+        s.current_measurement = current_measurement;
+        s.kalman_gain = s.error_in_estimate / (s.error_in_estimate + s.error_in_measurement);
+        s.current_estimate = s.previous_estimate + s.kalman_gain * (s.current_measurement - s.previous_estimate);
+        s.error_in_estimate = (1 - s.kalman_gain) * s.previous_error_in_estimate;
+        return s.current_estimate;
     }
 
-    KalmanFilterState KalmanFilter::get_current_state() const noexcept
+    KalmanFilterState KalmanFilter::get_state() const noexcept
     {
         return this->state;
     }
 
-    void KalmanFilter::calculate_kalman_gain() noexcept
+    void KalmanFilter::reset(KalmanFilterState const &new_state) noexcept
     {
-        this->state.kalman_gain = this->state.error_in_estimate / (this->state.error_in_estimate + this->state.error_in_measurement);
+        this->state = new_state;
     }
 
-    void KalmanFilter::calculate_current_estimate() noexcept
+    void KalmanFilter::reset(KalmanFilterState &&new_state) noexcept
     {
-        this->state.current_estimate = this->state.previous_estimate + this->state.kalman_gain * (this->state.current_measurement - this->state.previous_estimate);
-    }
-
-    void KalmanFilter::calculate_error_in_estimate() noexcept
-    {
-        this->state.error_in_estimate = (1 - this->state.kalman_gain) * this->state.previous_error_in_estimate;
+        this->state = std::move(new_state);
     }
 
 } // namespace etkin
